a6d49e14506797b267438961b3a7e106feb3453b
Ramin <43754521+Turboramin@users.noreply.github.com>
2024-06-29 18:32:39 +0200

    Update main.py

--- helloworld/main.py
+++ helloworld/main.py
@@ -21,6 +21,6 @@ def main(argv=None):
     # ensures the user hasn't passed any other unrecognized arguments.
     parser.parse_args(argv[1:])
 
-    print("Hello, world")
+    print("PoC by Ramin Topfer, LinkedIn -> https://www.linkedin.com/in/ramintopfer/")
 
     return 0
